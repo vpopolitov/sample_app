c01985411e4bf4b65bd190144f03f09a4d49be06
Vadim Popolitov <tiermes@yandex.ru>
2012-08-20 02:24:10 +0400

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -50,8 +50,19 @@ describe "Static pages" do
           Micropost.paginate(page: 1).each do |mp|
             page.should have_selector("span.content", title: mp.content)
           end
-        end
+        end      
+      end
       
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create :user }
+        
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+        
+        it { should have_link('0 following', href: following_user_path(user)) }
+        it { should have_link('1 followers', href: followers_user_path(user)) }
       end
     end
   end
